5c433955ab8bb0d5defc833aaa565b7e6c43930a
Seitu <seitujoseph@gmail.com>
2020-01-02 00:01:20 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const openNav = () => {
+  document.getElementById("mobile-menu").style.width = "100%";
+};
+
+const closeNav = () => {
+  document.getElementById("mobile-menu").style.width = "0%";
+};
